def81913c5e488178b4d4878428f89f0c0ee01e5
alex-1312 <alexroth@mail.de>
2022-01-01 21:36:48 +0100

    added bootstrap cdn and nav

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,9 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title><?= !empty($page) ? ucfirst($page) : 'Home' ?></title>
+
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <!-- jQuery library -->
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <!-- Popper JS -->
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <!-- Latest compiled JavaScript -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script> 
+
 </head>
 <body>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <!-- Brand -->
+  <a class="navbar-brand" href="#">Small Blog</a>
+
+  <!-- Toggler/collapsibe Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Navbar links -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item <?= empty($page) ? 'active' : '';  ?>">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item <?= ($page == 'link2') ? 'active' : ''; ?>">
+        <a class="nav-link" href="index.php?page=link2">Link2</a>
+      </li>
+      <li class="nav-item <?= ($page == 'link3') ? 'active' : ''; ?>">
+        <a class="nav-link" href="index.php?page=link3">Link3</a>
+      </li>
+    </ul>
+  </div>
+</nav> 
