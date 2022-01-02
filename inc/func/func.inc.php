35c567ef5692d25f97d0d6786512ae0306f0f5de
alex-1312 <alexroth@mail.de>
2022-01-02 13:52:06 +0100

    added functions, added secure to home and func

--- inc/func/func.inc.php
+++ inc/func/func.inc.php
@@ -2,23 +2,22 @@
 // avoid file from beeing called directly via url
 defined('SECURE') or die(header('location: ../../index.php'));
 
-/*
-  format a datestring coming from database
-  @param: $dbDate datestring from database
-  @param: $format (optional) pattern for string encoding 
-  @return: returns a utf8 encoded date string
+/**
+ * format a datestring coming from database
+ * @param: $dbDate datestring from database
+ * @param: $format (optional) pattern for string encoding 
+ * @return: returns a utf8 encoded date string
 */
-
 function formatDbDate($dbDate, $format = '%A, %d.%B.%Y %H:%M:%S')
 {
   return utf8_encode(strftime($format, strtotime($dbDate)));
 }
 
-/*
-  function to clean up user input
-  @param: $userInput string
-  @param: $encoding (optional) 
-*/
+/**
+ * function to clean up user input
+ * @param: $userInput string
+ * @param: $encoding (optional)
+ */
 function cleanInput($userInput, $encoding='UTF-8')
 {
   return htmlspecialchars(
@@ -27,12 +26,74 @@ function cleanInput($userInput, $encoding='UTF-8')
     $encoding
   );
 }
-
-/*
-  function to redirect to given url
-*/
+/**
+ * function to redirect to given url 
+ * @param: $url - url to redirect to
+ */
 function redirect($url)
 {
   header('location: ' . $url);
   exit;
+}
+
+/**
+ * function to determine if user is logged in 
+ * @return: bool 
+ */
+function isLoggedIn()
+{
+    return isset($_SESSION['logged']);
+}
+
+/**
+ * function to determine if user has blog_user rights
+ * @return: bool 
+ */
+function isBlogUser()
+{
+  if(isset($_SESSION['logged']) && $_SESSION['role'] === 'blog_user'){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+/**
+ * function to determine if user has admin user rights
+ * @return: bool 
+ */
+function isAdminUser()
+{
+  if(isset($_SESSION['logged']) && $_SESSION['role'] === 'admin'){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+/**
+ * function to log user in i.e. set the session vars
+ * @param: $mail - string
+ * @param: $username - string
+ * @param: $id - string
+ * @param: $role - string 
+ */
+function logIn($mail, $username, $id, $role)
+{
+  $_SESSION['logged'] = $mail;
+  $_SESSION['username'] = $username;
+  $_SESSION['id'] = $id;
+  $_SESSION['role'] = $role;
+}
+
+/**
+ * function to log off the user i.e. unset the session vars
+ */
+function logOff()
+{
+  unset($_SESSION['logged']);
+  unset($_SESSION['username']);
+  unset($_SESSION['id']);
+  unset($_SESSION['role']);
+  unset($_SESSION['token']);
 }
--- inc/home/home.inc.php
+++ inc/home/home.inc.php
@@ -1,3 +1,7 @@
+<?php
+// prevent file from beeing called directly via url
+defined('SECURE') or die(header('location: ../../index.php'));
+?>
 <div class="container text-center pt-3" >
   <h1>HOME.INC.PHP</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos tempora iusto similique perspiciatis, laudantium corrupti, facere debitis earum dolore obcaecati dolor rem! Aliquid quidem cumque expedita obcaecati nulla cupiditate dolor.</p>
