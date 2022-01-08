1a7cec717ef521bc7c9b32883f0dbef12655b24c
alex-1312 <alexroth@mail.de>
2022-01-08 20:12:37 +0100

    small changes

--- js/windowsize.js
+++ js/windowsize.js
@@ -4,16 +4,12 @@ function addDiv()
   let body = document.querySelector("body");
   windowSize.style.color = "red";
   windowSize.style.position = "absolute";
-  windowSize.style.top = "5px";
-  windowSize.style.left = "5px";
+  windowSize.style.top = "0px";
+  windowSize.style.left = "0px";
   windowSize.id = "ws";
   body.appendChild(windowSize);
-
-  let para = document.createElement("p");
-  para.id = "para";
-  windowSize.appendChild(para);
 };
 addDiv();
 window.addEventListener("resize", function(){
-  document.getElementById("para").innerText = 'WIDTH: ' + window.innerWidth + ' HEIGHT: ' + window.innerHeight;
+  document.getElementById("ws").innerText = 'WIDTH: ' + window.innerWidth + ' HEIGHT: ' + window.innerHeight;
 });
