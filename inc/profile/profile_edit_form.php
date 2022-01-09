78af10b14f10ebbfdae9221d90e0d5248bafa79e
alex-1312 <alexroth@mail.de>
2022-01-09 11:23:19 +0100

    added form

--- inc/profile/profile_edit_form.php
+++ inc/profile/profile_edit_form.php
@@ -1,13 +1,60 @@
-<?php
-// for development purposes only
-// enable full error reporting
-error_reporting(E_ALL);
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
+<form class="needs-validation" action="inc/profile/profile_edit.php" method="post" novalidate>
+            
+  <!-- Firstname -->
+  <div class="form-group">
+    <label for="firstname">Vorname</label>
+    <input  type="text" class="form-control" 
+            pattern="[A-Za-z]{3,}" value="<?= $firstName; ?>" 
+            id="firstname" name="firstname" required>
+    <div class="valid-feedback">Ok.</div>
+    <div class="invalid-feedback">
+      Bitte füllen Sie dieses Feld aus. Mindestens 3 Buchstaben.
+    </div>
+  </div>
 
-// includes
-require_once '../db/db.inc.php';
-require_once '../func/func.inc.php';
+  <!-- Lastname-->
+  <div class="form-group">
+    <label for="lastname">Nachname</label>
+    <input  type="text" class="form-control" 
+            pattern="[A-Za-z]{3,}" value="<?= $lastName; ?>" 
+            id="lastname" name="lastname" required>
+    <div class="valid-feedback">Ok.</div>
+    <div class="invalid-feedback">
+      Bitte füllen Sie dieses Feld aus. Mindestens 3 Buchstaben.
+    </div>
+  </div>
 
-// start session
-session_start();
+  <!-- EMAIL -->
+  <div class="form-group">
+    <label for="email">E-Mail Adresse</label>
+    <input  type="email" class="form-control" 
+            pattern="^[A-Za-z]*[\._-]?[A-Za-z]*@{1}[A-Za-z]{2,}[\.]{1}[A-Za-z]{2,}[\.]?[A-Za-z]{0,}$" 
+            value="<?= $eMail ?>" 
+            id="email" name="email" required>
+    <div class="valid-feedback">Ok.</div>
+    <div class="invalid-feedback">
+      Bitte füllen Sie dieses Feld mit einer gültigen E-Mail Adresse aus.
+    </div>
+  </div>
+
+  <!-- PASSWORD -->
+  <div class="form-group">
+    <label for="password">Passwort</label>
+    <input  type="password" class="form-control" 
+            pattern="^.{8,}$" 
+            id="password" name="password">
+    <div class="valid-feedback">Ok.</div>
+    <div class="invalid-feedback">
+      Wenn Sie ihr Passwort ändern wollen, sollte dieses aus mindestens 8 Zeichen bestehen.
+    </div>
+  </div>
+
+  <input type="hidden" name="xsrf-token" value="<?= $_SESSION['token']; ?>">
+
+  <!-- Actionbuttons -->
+  <div class="">
+    <button type="button" class="btn btn-danger" data-dismiss="modal">Zurück</button>
+    <button type="submit" class="btn btn-primary">Abschicken</button>
+  </div>
+
+</form>
